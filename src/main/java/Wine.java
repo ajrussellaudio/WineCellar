a049f33c814e92b2e905dbb556ed3888f78a0b02
Alan Russell <ajrussellaudio@gmail.com>
2018-05-30 22:41:09 +0100

    add more tests

--- src/main/java/Wine.java
+++ src/main/java/Wine.java
@@ -1,21 +1,3 @@
 public class Wine {
-    private String name;
-    private String grape;
 
-    public Wine(String name, String grape) {
-        this.name = name;
-        this.grape = grape;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGrape() {
-        return grape;
-    }
 }
--- src/test/java/WineTest.java
+++ src/test/java/WineTest.java
@@ -1,5 +1,4 @@
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -9,7 +8,7 @@ public class WineTest {
 
     @Before
     public void setUp() {
-        wine = new Wine("La Rioja Alta", "Tempranillo");
+        wine = new Wine("La Rioja Alta", "Tempranillo", 2007);
     }
 
     @Test
@@ -17,14 +16,24 @@ public class WineTest {
         assertEquals("La Rioja Alta", wine.getName());
     }
 
-    @Test
-    public void canSetName() {
-        wine.setName("La Rioja Alta Gran Reserva");
-        assertEquals("La Rioja Alta Gran Reserva", wine.getName());
-    }
-
-    @Test
-    public void hasGrape() {
-        assertEquals("Tempranillo", wine.getGrape());
-    }
+//    @Test
+//    public void canSetName() {
+//        wine.setName("La Rioja Alta Gran Reserva");
+//        assertEquals("La Rioja Alta Gran Reserva", wine.getName());
+//    }
+//
+//    @Test
+//    public void hasGrape() {
+//        assertEquals("Tempranillo", wine.getGrape());
+//    }
+//
+//    @Test
+//    public void hasVintage() {
+//        assertEquals(2007, wine.getVintage());
+//    }
+//
+//    @Test
+//    public void canGetAgeInYears() {
+//        assertEquals(11, wine.age());
+//    }
 }
